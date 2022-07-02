72f0913c644cb39b0bb592d1136e952ef37b21c1
mohitb07 <bmohit980@gmail.com>
2022-07-02 22:12:31 +0530

    code refactored

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,13 @@
 import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../firebase/clientApp';
 import RightContent from './RightContent/RightContent';
 import SearchInput from './SearchInput';
 
 
 const Navbar:React.FC = () => {
-    
+    const [user, loading, error] = useAuthState(auth)
     return (
         <Flex bg="white" height="44px" padding="6px 12px">
             <Flex align="center">
@@ -13,7 +15,7 @@ const Navbar:React.FC = () => {
                 <Image src='/images/redditText.svg' height="46px" alt="reddit text" display={{base: 'none', md: 'unset'}}/>
             </Flex>
             <SearchInput/>
-            <RightContent/>
+            <RightContent user={user}/>
         </Flex>
     )
 }
