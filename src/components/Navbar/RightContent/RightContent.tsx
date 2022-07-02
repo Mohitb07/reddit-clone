402cdcd511721e3b17b2977cc1dd426b885e9f95
mohitb07 <bmohit980@gmail.com>
2022-07-02 22:13:15 +0530

    code refactored

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,19 +1,22 @@
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
+import { signOut, User } from 'firebase/auth';
 import React from 'react';
+import { auth } from '../../../firebase/clientApp';
 import AuthModal from '../../Modal/Auth/AuthModal';
 import AuthButtons from './AuthButtons';
+import Icons from './Icons';
 
 type RightContentProps = {
-    
+    user?: User | null
 };
 
-const RightContent:React.FC<RightContentProps> = () => {
+const RightContent:React.FC<RightContentProps> = ({user}) => {
     
     return (
         <>
             <AuthModal/>
             <Flex justify="center" align="center">
-                <AuthButtons/>
+                {user ? <Icons/> : <AuthButtons/>}
             </Flex>
         </>
     )
