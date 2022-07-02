6a538530a94b77bbeae4190c3301fb37dc3cb366
mohitb07 <bmohit980@gmail.com>
2022-07-02 22:11:09 +0530

    Restructured data flow

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,11 +1,17 @@
-import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure } from '@chakra-ui/react';
-import React from 'react';
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure, Flex, Text } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRecoilState } from 'recoil';
 import { authModalState } from '../../../atoms/authModalAtom';
+import { auth } from '../../../firebase/clientApp';
+import AuthInputs from './AuthInputs';
+import OAuthButtons from './OAuthButtons';
+import ResetPassword from './ResetPassword';
 
 
 const AuthModal:React.FC = () => {
     const [modalState, setModalState] = useRecoilState(authModalState)
+    const [user, loading, error] = useAuthState(auth)
 
     const handleClose = () => {
         setModalState(prev => ({
@@ -13,16 +19,40 @@ const AuthModal:React.FC = () => {
             open: false,
         }))
     }
+
+    // useEffect(() => {
+    //   if(user) handleClose()
+    //   console.log('user', user)
+    // }, [user])
+
+    const toggleView = (view: string) => {
+      setModalState({
+        ...modalState,
+        view: view as typeof modalState.view,
+      });
+    };
     
     return (
         <>
           <Modal isOpen={modalState.open} onClose={handleClose}>
             <ModalOverlay />
             <ModalContent>
-              <ModalHeader>Modal Title</ModalHeader>
+              <ModalHeader textAlign="center">
+                {modalState.view === 'login' && "Login"}
+                {modalState.view === 'signup' && "Sign Up"}
+                {modalState.view === 'resetPassword' && "Reset Password"}
+              </ModalHeader>
               <ModalCloseButton />
-              <ModalBody>
-                Here is the custom modal text
+              <ModalBody pb={6} display="flex" flexDirection="column" alignItems="center" justifyItems="center">
+                <Flex direction="column" align="center" justify="center" width="70%">
+                {modalState.view === 'login' || modalState.view === 'signup' ? (
+                  <>
+                    <OAuthButtons/>
+                    <Text color="gray.400" fontWeight="700">OR</Text>
+                    <AuthInputs toggleView={toggleView}/>
+                  </>         
+                  ) : <ResetPassword toggleView={toggleView}/>}
+                </Flex>
               </ModalBody>
             </ModalContent>
           </Modal>
